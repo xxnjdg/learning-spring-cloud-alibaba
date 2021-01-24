af590f0e4926886789fc115eafeb448f7b68044e
xxnjdg <1422570468@qq.com>
2021-01-25 06:42:36 +0800

    增加了nacos-example-one例子

---
+++ learning-spring-cloud-alibaba-nacos/nacos-example-one/src/main/java/io/github/xxnjdg/nacos/example/one/NacosExampleOne.java
@@ -0,0 +1,15 @@
+package io.github.xxnjdg.nacos.example.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ghzj
+ * @date 2021/1/25 21:25
+ */
+@SpringBootApplication
+public class NacosExampleOne {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosExampleOne.class,args);
+    }
+}
